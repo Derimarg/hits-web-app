17e668a10606f1a41668067c03ce4336b8854260
Derimar Gray <derimargray@gmail.com>
2022-08-11 11:40:37 -0500

    added sidebar component and styles

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Topbar from "../../components/Topbar/Topbar";
+import Sidebar from '../../components/Sidebar/Sidebar'
 import Feed from '../../components/Feed/Feed';
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
         <>
             <Topbar />
             <div style={{ display: 'fles', width: '100%' }}>
+                <Sidebar />
                 <Feed />
             </div>
         </>
