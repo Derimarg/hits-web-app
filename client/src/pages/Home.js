6173697c6f414c1053d0d579525f76ebe709c05a
Derimar Gray <derimargray@gmail.com>
2022-08-11 06:39:23 -0500

    added frined list component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PostList from '../components/PostList';
 import PostForm from '../components/PostForm';
-
+import FriendList from '../components/FriendList';
 
 import Auth from '../utils/auth';
 import { useQuery } from '@apollo/client';
@@ -32,7 +32,15 @@ const Home = () => {
                         />
                     )}
                 </div>
-
+                {loggedIn && userData ? (
+                    <div className="col-12 col-lg-3 mb-3">
+                        <FriendList
+                            username={userData.me.username}
+                            friendCount={userData.me.friendCount}
+                            friends={userData.me.friends}
+                        />
+                    </div>
+                ) : null}
             </div>
         </main>
     );
