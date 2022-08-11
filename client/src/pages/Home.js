1fe4dcfcfce1701308d2f4ae0278fd16ced33100
Derimar Gray <derimargray@gmail.com>
2022-08-11 06:34:19 -0500

    added post form component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,29 @@
-import React from 'react'
+import React from 'react';
+import PostForm from '../components/PostForm';
+
+import Auth from '../utils/auth';
+import { useQuery } from '@apollo/client';
+import { QUERY_POST, QUERY_ME_BASIC } from '../utils/queries';
 
 const Home = () => {
+    const { loading, data } = useQuery(QUERY_POST);
+    const { data: userData } = useQuery(QUERY_ME_BASIC);
+    const posts = data?.posts || [];
+
+    const loggedIn = Auth.loggedIn();
+
     return (
-        <div >
-            <header >
-                <p>
-                    Welcome to Harvard in Tech
-                </p>
+        <main>
+            <div className="flex-row justify-space-between">
+                {loggedIn && (
+                    <div className="col-12 mb-3">
+                        <PostForm />
+                    </div>
+                )}
+                
+            </div>
+        </main>
+    );
+};
 
-            </header>
-        </div>
-    )
-}
-
-export default Home
+export default Home;
