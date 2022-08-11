151b4d7479eba8d4935fc76c3b115ca3fa007cd5
Derimar Gray <derimargray@gmail.com>
2022-08-11 07:11:28 -0500

    added login screen

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
+import Login from './pages/Loging';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -43,6 +44,10 @@ function App() {
               <Route
                 path="/"
                 element={<Home />}
+              />
+              <Route
+                path="/login"
+                element={<Login />}
               />
             </Routes>
           </div>
