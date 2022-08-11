626cde1dd39e7ed6f013164aa5f22689f09bd9d9
Derimar Gray <derimargray@gmail.com>
2022-08-11 11:27:16 -0500

    added feed container component and styles

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Topbar from "../../components/Topbar/Topbar";
+import Feed from '../../components/Feed/Feed';
 
 export default function Home() {
     return (
         <>
             <Topbar />
             <div style={{ display: 'fles', width: '100%' }}>
-               
+                <Feed />
             </div>
         </>
     );
