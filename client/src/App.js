a101d91589356bfdbdfb24a928f3778aef66313c
Derimar Gray <derimargray@gmail.com>
2022-08-11 07:16:05 -0500

    added signup screen and api request

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,8 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
-import Login from './pages/Loging';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -48,6 +49,10 @@ function App() {
               <Route
                 path="/login"
                 element={<Login />}
+              />
+              <Route
+                path="/signup"
+                element={<Signup />}
               />
             </Routes>
           </div>
