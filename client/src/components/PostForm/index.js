c01c9aaa9de17fb40e82d5434923d8fc6c90f720
Derimar Gray <derimargray@gmail.com>
2022-08-11 13:18:42 -0500

    update users

--- client/src/components/PostForm/index.js
+++ client/src/components/PostForm/index.js
@@ -34,7 +34,7 @@ const PostForm = () => {
 
   // update state based on form input changes
   const handleChange = (event) => {
-    if (event.target.value.length <= 1000) {
+    if (event.target.value.length <= 280) {
       setText(event.target.value);
       setCharacterCount(event.target.value.length);
     }
@@ -59,12 +59,12 @@ const PostForm = () => {
 
   return (
     <div>
-      {/* <p
-        className={`m-0 ${characterCount === 1000 || error ? 'text-error' : ''}`}
+      <p
+        className={`m-0 ${characterCount === 280 || error ? 'text-error' : ''}`}
       >
-        Character Count: {characterCount}/1000
+        Character Count: {characterCount}/280
         {error && <span className="ml-2">Something went wrong...</span>}
-      </p> */}
+      </p>
       <form
         className="flex-row justify-center justify-space-between-md align-stretch"
         onSubmit={handleFormSubmit}
